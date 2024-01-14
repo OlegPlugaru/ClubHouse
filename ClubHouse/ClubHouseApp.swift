e2861bd1fa18e321f768f5a282f008cf57be0997
OlegPlugaru <olegplugaru.md@gmail.com>
2024-01-14 18:24:27 +0200

    Initial Commit

---
+++ ClubHouse/ClubHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubHouseApp.swift
+//  ClubHouse
+//
+//  Created by Oleg Plugaru on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClubHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
